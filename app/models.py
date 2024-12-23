3ceabdd347cce3620ded3b18d12f46eb979e5a02
coder-w <phoenixedian@gmail.com>
2024-12-23 11:19:03 +0300

    reverted changes found mistakes

--- app/models.py
+++ app/models.py
@@ -58,3 +58,8 @@ class Concert(Base):
      band = relationship('Band', back_populates='concerts')
      venue = relationship('Venue', back_populates='concerts')
      
+     def hometown_show(self):
+        return self.venue.city == self.band.hometown
+
+     def introduction(self):
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
